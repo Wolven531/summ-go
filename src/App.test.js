9306ff68f1f7bc1c59803cc1d68b08c3cbe899a5
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-16 18:22:50 -0700

    Update application unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 
-it('renders without crashing', () => {
-	const div = document.createElement('div')
-	ReactDOM.render(<App />, div)
-	ReactDOM.unmountComponentAtNode(div)
+import { App } from './App'
+
+describe('Summ Go application', () => {
+	let applicationElement
+
+	beforeAll(() => {
+		applicationElement = document.createElement('div')
+	})
+
+	it('renders without crashing', () => {
+		ReactDOM.render(<App />, applicationElement)
+	})
+
+	afterAll(() => {
+		ReactDOM.unmountComponentAtNode(applicationElement)
+	})
 })
