669787ebc9c6819099ee66d67a123a2f88ec67f1
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-16 18:14:55 -0700

    Introduce MonsterManager

---
+++ src/MonsterManager.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './MonsterManager.css'
+
+class MonsterManager extends Component {
+	render() {
+		return (
+			<article className="monster-manager">
+				<h2>Monster Manager</h2>
+			</article>
+		)
+	}
+}
+
+export { MonsterManager }
