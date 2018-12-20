e1383af614b18a0c4ff9b7422de84225fe3e6e67
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-19 18:51:50 -0700

    Add test for name input

--- src/MonsterManager.test.js
+++ src/MonsterManager.test.js
@@ -11,4 +11,20 @@ describe('Monster Manager componenet', () => {
 		fixture = shallow(<MonsterManager />)
 		expect(fixture).toMatchSnapshot()
 	})
+
+	it('renders monster addition form properly', () => {
+		fixture = shallow(<MonsterManager />)
+		const monsterForm = fixture.find('form')
+
+		expect(monsterForm.exists()).toBeTruthy()
+
+		const nameInput = monsterForm.find('input#mon-name')
+		const nameProps = nameInput.props()
+
+		expect(nameProps.id).toEqual('mon-name')
+		expect(nameProps.name).toEqual('mon-name')
+		expect(nameProps.type).toEqual('text')
+		expect(nameProps.value).toEqual('')
+		expect(nameProps.onChange).toBeDefined()
+	})
 })
