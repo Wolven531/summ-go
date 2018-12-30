8200e7b5154e048212caeb9ceb59896a02ad767a
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-30 16:31:19 -0500

    Enhance setupTests with comments explaining jest-localstorage-mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+
 import 'jest-localstorage-mock'
+// NOTE: Dec-30-2018, ajw
+// For more info, see https://www.npmjs.com/package/jest-localstorage-mock
 
 configure({ adapter: new Adapter() })
