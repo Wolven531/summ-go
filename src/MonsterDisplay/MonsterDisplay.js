5271fc7f627a4d5a059b1894aa32dd7101baa006
Anthony Williams <anthony.williams.cs@gmail.com>
2019-01-10 22:24:24 -0700

    Add basic CSS class for selector access

--- src/MonsterDisplay/MonsterDisplay.js
+++ src/MonsterDisplay/MonsterDisplay.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 // import { Monster } from '../Models/Monster'
 
 const MonsterDisplay = ({ monster }) => (
-	<article>
+	<article className="monster-display">
 		<h3>{monster.name}</h3>
 		<section>
 			{monster.stars}
