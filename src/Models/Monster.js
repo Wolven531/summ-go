b38de0ec7cb89ef6afcb5a9874254c5854efa758
Anthony Williams <anthony.williams.cs@gmail.com>
2019-01-26 20:54:29 -0700

    Add comment for future props on Monster model

--- src/Models/Monster.js
+++ src/Models/Monster.js
@@ -1,4 +1,19 @@
 class Monster {
+	/*
+		"awakenedName"
+		"earlyRuneList"
+		"earlyRuneValues"
+		"element"
+		"lateRuneList"
+		"lateRuneValues"
+		"name"
+		"rating"
+		"statPriority"
+		"awakenedImage"
+		"awakenedImageSource"
+		"image"
+		"imageSource"
+	*/
 	constructor(name, element, stars) {
 		this.name = name
 		this.element = element
