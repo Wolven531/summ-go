d692dc95c62f6700f6a08b925044de76fd416c3a
Anthony Williams <anthony.williams.cs@gmail.com>
2019-02-01 22:40:37 -0700

    Fix addMonster to use model constructor

--- src/MonsterManager/MonsterManager.js
+++ src/MonsterManager/MonsterManager.js
@@ -138,7 +138,11 @@ class MonsterManager extends Component {
 			return
 		}
 
-		const newMonster = new Monster(this.state.monsterName, this.state.monsterElement, this.state.monsterStars)
+		const newMonster = new Monster(
+			this.state.monsterName,
+			this.state.monsterName,
+			this.state.monsterElement,
+			this.state.monsterStars)
 		const updatedMonsters = [...this.state.monsters].concat(newMonster)
 
 		this.setState({
