0bd9feb48693c35a08810e4792023192fdc0231e
Anthony Williams <anthony.williams.cs@gmail.com>
2019-02-03 20:41:23 -0700

    Implement first site layout

--- src/App/App.js
+++ src/App/App.js
@@ -8,8 +8,18 @@ class App extends Component {
 	render() {
 		return (
 			<article className="app">
-				<h1>Summ Go</h1>
-				<MonsterManager />
+				<div class="header">
+					<h1>Summ Go</h1>
+				</div>
+				<div class="sidebar">
+					sidebar
+				</div>
+				<div class="content-1">
+					<MonsterManager />
+				</div>
+				<div class="content-2">Content-2</div>
+				<div class="content-3">Content-3</div>
+				<div class="footer">&copy; Anthony Williams 2019</div>
 			</article>
 		)
 	}
